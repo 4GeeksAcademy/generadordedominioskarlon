2a05b679aaf56167fd1991557c413ba6692527db
karlon2022 <99146020+karlon2022@users.noreply.github.com>
2026-05-23 01:50:57 +0000

    mi generador de dominios listo karlon

---
+++ app.js
@@ -0,0 +1,13 @@
+  // sentencias
+  let pronoun = ['el', 'su', 'este'];
+  let adj = ['mejor', 'gran', 'nuevo', 'simple'];
+  let noun = ['carro', 'robot', 'video', 'restaurante', 'buscador'];
+  
+//Dominios aleatorios posibles
+  for(let letra1 of pronoun) {
+    for(let letra2 of adj) {
+      for(let letra3 of noun) {
+       console.log(letra1 + letra2 + letra3 + '.com'); 
+      }
+    }
+  }
